45bc827aae3d1d3621e5f2ab4950f367295b31f9
Enrique <eopqng@gmail.com>
2014-11-09 20:56:28 +0100

    nada nuevo

--- Segeca/src/ui/Controller.java
+++ Segeca/src/ui/Controller.java
@@ -2,10 +2,24 @@ package ui;
 
 public class Controller {
 
+	static Conector.ConectorBD bd;
+	
 	public static void main(String[] args) {
 		
 		UI window = new UI();
 		window.getFrame().setVisible(true);
+		
+		bd = new Conector.ConectorBD();
+		pruebas();
+		bd.desconectar();
+	}
+	
+	private static void pruebas(){
+		def.Persona p = new def.Persona();
+		p.setNick("Javier");
+		bd.extraerPersona(p);
+		System.out.println(p.toString());
+		
 	}
 
 }
